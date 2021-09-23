8996033f957b23a21062d8f559c26309ff11e23f
Tyler O'Brien <hello@tylerobrien.com>
2021-09-23 12:22:46 -0400

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  env: {
+    es6: true
+  },
+  parserOptions: {
+    sourceType: 'module'
+  },
+  plugins: [
+    'eslint-comments',
+  ],
+};
---
+++ README.md
@@ -0,0 +1,10 @@
+# TypeScript Sandbox
+
+This is an npm project configured for quickly running and/or building TypeScript.
+
+# Usage
+
+```bash
+npm run exec -- hello.ts
+npm run build
+```
